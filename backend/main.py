0d5d8571c63963924d7c8b99c33d46829e6bd198
Mohd Aqdas Asim <143924496+MohdAqdasAsim@users.noreply.github.com>
2025-03-28 12:20:35 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -20,10 +20,3 @@ app.add_middleware(
 
 # Include the API router
 app.include_router(router)
-
-import uvicorn
-import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # Set 8080 as default
-    uvicorn.run(app, host="0.0.0.0", port=port)
